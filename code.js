6a406718e38ec07ff063f077a44067d74956bce4
omojhejhe <68550575+omojhejhe@users.noreply.github.com>
2022-06-24 00:59:03 +0100

    My first javascript code showing my name, height and country

---
+++ code.js
@@ -0,0 +1,10 @@
+let name = 'Odepidan Omojola'
+let height = 5.6
+let country = 'Nigeria'
+alert(name);
+alert(height);
+alert(country);
+
+console.log(name);
+console.log(height);
+console.log(country);
